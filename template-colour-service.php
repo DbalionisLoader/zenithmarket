97e35dfb19a8b86ca22ddf2d3050bf5f65bfc68b
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2024-07-20 23:14:18 +0100

    Add colour service page and hero section

--- template-colour-service.php
+++ template-colour-service.php
@@ -7,7 +7,23 @@ get_header();?>
 
 <div id="primary" class="">
     <main id="main" class="">
-        <h2>Service Work</h2>
+        <section class="cmm-hero-section" >
+            <picture>
+                 <source srcset="<?php echo get_template_directory_uri(); ?>/images/carhero-road-2100x800-pg.png" media="(min-width: 768px)">
+                 <source srcset="<?php echo get_template_directory_uri(); ?>/images/carheroimage-mobile-cwebp-600x536.webp" media="(min-width: 480px)">
+                 <img src="<?php echo get_template_directory_uri(); ?>/images/carheroimage-mobile-cwebp-600x536.webp" alt="Red sport's car hood" class="cmm-hero-image">
+            </picture>
+            <div class="cmm-hero-container">   
+            <div class="cmm-hero-content">
+                <span class="cmm-preheading">Soll Automotive Paints</span> <br>
+                <h1>Car Paint Matching</h1>
+                <p>Get the most accurate paint colour match and specialty car paint mixed at Soll Leeds. </p>
+                <div class="cmm-buttons-container">
+                    <a href="#" class="cmm-button primary">Visit store</a>
+                </div>
+            </div>
+            </div> 
+        </section>
     </main>
     
 </div>
